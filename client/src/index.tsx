cbd620666d3ac4f3dcebd408c727c9423d8038ac
radchukd <radchuk.d@gmail.com>
2020-04-18 16:29:58 +0200

    remove blank line

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { apolloClient } from './config';
 import AppRouter from './router';
 
-
 render(
   <ApolloProvider client={apolloClient}>
     <BrowserRouter>
